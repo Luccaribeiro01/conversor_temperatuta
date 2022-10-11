8dfa991e7be3bee719b1cf88d6d44c0a3f955424
Lucca <luccaribeiro.work@gmail.com>
2022-10-11 19:45:04 -0300

    conversor_temperatura

---
+++ conversor_temperatura/bin/conversor_temperatura.dart
@@ -0,0 +1,11 @@
+
+
+void main() {
+
+final celsius = 32;
+
+final farenheit = ((9*celsius) + 160) / 5;
+
+print(farenheit);
+print('$farenheit °F');
+}
---
+++ conversor_temperatura/test/conversor_temperatura_test.dart
@@ -0,0 +1,8 @@
+import 'package:conversor_temperatura/conversor_temperatura.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
